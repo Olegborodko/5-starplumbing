b1c0929e88e2097ca07537ea6d9a4cc20d55ce56
RushDev <>
2022-07-22 04:53:05 +0000

    push to test site

--- wp-content/themes/plumbio3333/header.php
+++ wp-content/themes/plumbio3333/header.php
@@ -17,6 +17,79 @@
 	<meta name="format-detection" content="telephone=no">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<?php wp_head(); ?>
+	<?php $curUrl = 'http'.((isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=='off')?'s':'').'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
+	<script type="application/ld+json">
+		{
+		  "@context": "https://schema.org",
+		  "@type": "Plumber",
+		  "name": "5 Star Plumbing",
+		  "image": "https://5-starplumbing.com/wp-content/uploads/2021/06/LOGO-WEB-1.png",
+		  "@id": "https://schema.org/LocalBusiness",
+		  "url": "https://5-starplumbing.com/",
+		  "telephone": "(916)796-1233",
+		  "address": {
+			"@type": "PostalAddress",
+			"streetAddress": "Roseville Rd #109",
+			"addressLocality": "North Highlands",
+			"addressRegion": "CA",
+			"postalCode": "95660",
+			"addressCountry": "US"
+		  },
+		  "geo": {
+			"@type": "GeoCoordinates",
+			"latitude": 38.7020693,
+			"longitude": -121.3317341
+		  },
+		  "openingHoursSpecification": [{
+			"@type": "OpeningHoursSpecification",
+			"dayOfWeek": [
+			  "Monday",
+			  "Tuesday",
+			  "Wednesday",
+			  "Thursday",
+			  "Friday"
+			],
+			"opens": "07:00",
+			"closes": "18:00"
+		  },{
+			"@type": "OpeningHoursSpecification",
+			"dayOfWeek": [
+			  "Saturday",
+			  "Sunday"
+			],
+			"opens": "07:00",
+			"closes": "16:00"
+		  }],
+		  "sameAs": "https://www.facebook.com/5StarPlumbing/" 
+		}
+	</script>
+	<script type="application/ld+json">
+	{
+	  "@context": "https://schema.org/", 
+	  "@type": "Product", 
+	  "name": "5 Star Plumbing",
+	  "image": "https://5-starplumbing.com/wp-content/uploads/2021/06/LOGO-WEB-1.png",
+	  "description": "Certificated, skilled, and trustworthy premier plumbers. We provide quality, sustainability, and value. Contact us to receive a notch-carving service, repair, and adjustment.",
+	  "brand": {
+		"@type": "Brand",
+		"name": "5 Star Plumbing"
+	  },
+	  "sku": "111",
+	  "offers": {
+		"@type": "AggregateOffer",
+		"url": "<?php echo $curUrl; ?>",
+		"priceCurrency": "USD"
+	  },
+	  "aggregateRating": {
+		"@type": "AggregateRating",
+		"ratingValue": "4.9",
+		"bestRating": "5",
+		"worstRating": "1",
+		"ratingCount": "1882"
+	  }
+	}
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
